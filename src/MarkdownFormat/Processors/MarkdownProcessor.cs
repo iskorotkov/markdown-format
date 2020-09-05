18e07adc5168ec0187e6479c01a597f6884d1608
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-09-05 16:40:54 +0500

    Implement MarkdownProcessor

---
+++ src/MarkdownFormat/Processors/MarkdownProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarkdownFormat.Formatters;
+using MarkdownFormat.IO;
+
+namespace MarkdownFormat.Processors
+{
+    public class MarkdownProcessor : IMarkdownProcessor
+    {
+        private readonly IReader _reader;
+        private readonly IWriter _writer;
+        private readonly IEnumerable<IMarkdownFormatter> _formatters;
+
+        public MarkdownProcessor(IReader reader, IWriter writer, IEnumerable<IMarkdownFormatter> formatters)
+        {
+            _reader = reader;
+            _writer = writer;
+            _formatters = formatters;
+        }
+
+        public async Task RunAsync()
+        {
+            var line = await _reader.ReadLineAsync();
+            line = _formatters.Aggregate(line, (current, formatter) => formatter.Run(current));
+            await _writer.WriteLineAsync(line);
+        }
+    }
+}
--- src/MarkdownFormat/Program.cs
+++ src/MarkdownFormat/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Threading.Tasks;
+using MarkdownFormat.IO;
+using MarkdownFormat.Processors;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MarkdownFormat
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return await ConfigureCommandLineInterface()
+                .InvokeAsync(args);
+        }
+
+        private static RootCommand ConfigureCommandLineInterface()
+        {
+            var root = new RootCommand
+            {
+                Handler = CommandHandler.Create<string>(Run),
+            };
+
+            var fileArgument = new Argument("filename")
+            {
+                ArgumentType = typeof(FileInfo),
+                Arity = new ArgumentArity(0, 1),
+            };
+            root.AddArgument(fileArgument);
+
+            return root;
+        }
+
+        private static void Run(string? filename)
+        {
+            var services = new ServiceCollection();
+            services.AddScoped<IMarkdownProcessor, MarkdownProcessor>();
+            services.AddTransient<IReader>();
+            services.AddTransient<IWriter>();
+            var provider = services.BuildServiceProvider();
+            var processor = provider.GetRequiredService<IMarkdownProcessor>();
         }
     }
 }
